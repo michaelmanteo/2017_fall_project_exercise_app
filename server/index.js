e64ec13b5bfbf679c31a5f0b7383055fd69e3844
Michael N. Manteo <michaelmanteo@gmail.com>
2017-10-23 16:07:29 -0400

    added routes to calorie counter, login & signup. Simplified the navbar

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 // Server
 
 const express = require("express");
-const handler = require("./httpHandler")
+const handler = require("./httpHandler");
 const exerciseController = require("./exerciseController");
+const calorieController = require("./calorieController");
+const loginController = require("./loginController");
+const signupController = require("./signupController")
 const server = express();
 
-server.use("/", express.static("./jquery-mockup"));
-server.use("/old", handler.main);
-server.use("/exercise", exerciseController.router);
-
-    
-
-server.listen(3000);
+server
+    .use("/", express.static("./jquery-mockup"))
+    .use("/old", handler.main)
+    .use("/login", loginController.router)
+    .use("/signup", signupController.router)
+    .use("/calories", calorieController.router)
+    .use("/exercise", exerciseController.router)
+    .listen(3001);
 
 console.log("http://localhost:3000");
---
+++ server/loginController.js
@@ -0,0 +1,15 @@
+// Controller
+
+const express = require('express');
+const tracker = require('./trackerModel');
+const router = express.Router();
+var path = require('path');
+var html_dir = './jquery-mockup/';
+
+router.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, '../jquery-mockup', 'login.html'));
+});
+
+
+module.exports.router = router;
+
