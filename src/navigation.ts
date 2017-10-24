13af21328afbef9ca819adc85a6a038f7694586c
Michael N. Manteo <michaelmanteo@gmail.com>
2017-10-24 12:15:35 -0400

    setup webpack and typescript

---
+++ src/navigation.ts
@@ -0,0 +1,12 @@
+import * as $ from 'jquery';
+
+
+$(function () {
+    $("#header-placeholder").load("_header.html");
+});
+
+$(function () {
+    $("#footer-placeholder").load("_footer.html");
+});
+
+
